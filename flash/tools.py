cc13a7b200d6c0f0be76d8d54f7602e172e3fd7a
Nathan C. <149914029+Natuworkguy@users.noreply.github.com>
2026-08-05 10:36:42 -0700

    Fix Flake8 errors
    Signed-off-by: Nathan C. <149914029+Natuworkguy@users.noreply.github.com>

--- flash/tools.py
+++ flash/tools.py
@@ -51,10 +51,11 @@ def _shell_timeout(timeout) -> int:
 def shell_tool(command: str, timeout=None, is_user=False) -> str:
     """Tool to execute a shell command"""
 
-    if input("Flash is trying to execute the following command: \n" 
-             + command + 
-             "\nDo you approve it to run? [Y/N, default n]\n"
-            ).strip().lower() != "y":
+    if input(
+        "Flash is trying to execute the following command: \n"
+        + command +
+        "\nDo you approve it to run? [Y/N, default n]\n"
+    ).strip().lower() != "y":
         return "Command blocked by user"
 
     seconds = _shell_timeout(timeout)
